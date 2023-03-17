59479e7b6c523fa24bc3e0923ea8d3495e185244
권찬민 <kwonchanmin@gwonchanmin-ui-MacBookAir.local>
2023-03-17 10:22:14 +0900

    [#1]feat: master로 병합 작업 중

--- GymManna/src/main/java/com/mycompany/mygym/user/dao/UserDaoImpl.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.mygym.user.dao;
-
-import com.mycompany.mygym.user.vo.User;
-
-public interface UserDaoImpl {
-
-	User selectUser(User user);
-}
--- GymManna/src/main/java/com/mycompany/mygym/user/service/UserServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.mygym.user.service;
-
-import com.mycompany.mygym.user.vo.User;
-
-public interface UserServiceImpl {
-
-	User selectUser(User user);
-}
